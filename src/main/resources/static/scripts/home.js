5fcfe56fd3a7785a97bca98c5dfb23fb5d22d252
Sam Millner <Samuel.Millner@softwire.com>
2017-03-10 11:17:18 +0000

    Add jQuery and jQuery and UI sortable

--- src/main/resources/static/scripts/home.js
+++ src/main/resources/static/scripts/home.js
@@ -1,1 +1,23 @@
-alert("Hello world");
+$(function () {
+    $("#sortable").sortable({
+        connectWith: "ul"
+    });
+    $("#sortable2").sortable({
+        connectWith: "ul"
+    });
+    $("#sortable3").sortable({
+        connectWith: "ul"
+    });
+    $("#sortable4").sortable({
+        connectWith: "ul"
+    });
+    $("#sortable5").sortable({
+        connectWith: "ul"
+    });
+    $("#sortable6").sortable({
+        connectWith: "ul"
+    });
+    $("#sortable7").sortable({
+        connectWith: "ul"
+    });
+});
